c3c6e766cb9a5ce30e50ec5be34b0577ede8030d
Your Name <you@example.com>
2018-06-15 17:22:22 +0530

    java8 stream example

--- src/com/java8/stream/Java8StreamTest.java
+++ src/com/java8/stream/Java8StreamTest.java
@@ -1,64 +1,28 @@
 package com.java8.stream;
 
-import java.time.Year;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 public class Java8StreamTest {
-
+	
+/*	requirement is to find out whether the given phone number is matching or not with any of the user's phone number
+*/
 	public static void main(String[] args) {
 		
-	Item itm1= new Item(101,"headphone",2000);
-	Item itm2= new Item(101,"radio",2050);
-	Item itm3= new Item(101,"TV",20000);
-	Item itm4= new Item(101,"Computer",30000);
-	Item itm5= new Item(101,"Mobile",10000);
+	List<User> users=	Arrays.asList(
+			new User(101,"sam",Arrays.asList("984364677","78437437237")),
+			new User(102,"black",Arrays.asList("893474773","873887485")),
+			new User(102,"mike",Arrays.asList("788348374","783475848","8637465734"))
+			);
 	
-	Set<Item> itmSet1 = new HashSet<Item>();
-	itmSet1.add(itm1);
-	itmSet1.add(itm2);
+	Optional<String> result= users.stream().map(x->x.getPhoneNumbers().stream())
+			.flatMap(phoneNumberStream->phoneNumberStream.filter(
+					phonNumber->phonNumber.equals("783475848"))).findAny();
 	
-	Set<Item> itmSet2 = new HashSet<Item>();
-	itmSet1.add(itm3);
-	itmSet1.add(itm4);
+	//if values matches then only print
+	result.ifPresent(System.out::println);
 	
-	Set<Item> itmSet3 = new HashSet<Item>();
-	itmSet1.add(itm1);
-	itmSet1.add(itm5);
-	
-
-	Customer cust1 = new Customer(1001,"sam",itmSet1);
-	Customer cust2 = new Customer(1002,"black", itmSet2);
-	Customer cust3 = new Customer(1003,"mile" ,itmSet3);
-	
-	List<Customer> custList= new ArrayList<Customer>();
-	custList.add(cust1);
-	custList.add(cust2);
-	custList.add(cust3);
-	
-System.out.println(getEachCustTotalPrice(custList));
-		
-	}
-	
-	
-	public static Map<String, Integer> getEachCustTotalPrice(List<Customer> custList){
-		
-		/*Stream<Map<String,String>> priceMap= custList.stream().map(x->x.getItems())
-				.flatMap(y->y.stream()).collect(Collectors.toMap( Customer::getCustName, Item::getItemPrice));*/
-		
-		
-	Map<String, Integer> priceMap=  custList.stream().collect(Collectors.toMap(Customer::getCustName, x->x.getItems().stream().map(y->y.getItemPrice()).mapToInt(z->z).sum()));
-		
-		
-		return priceMap;		
-		
 	}
 
 }
---
+++ src/com/java8/stream/User.java
@@ -0,0 +1,71 @@
+package com.java8.stream;
+
+import java.util.List;
+
+public class User {
+
+	private int userId;
+	private String userName;
+	private  List<String> phoneNumbers;
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public List<String> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+	public void setPhoneNumbers(List<String> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+	public User() {
+		super();
+	}
+	public User(int userId, String userName, List<String> phoneNumbers) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.phoneNumbers = phoneNumbers;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((phoneNumbers == null) ? 0 : phoneNumbers.hashCode());
+		result = prime * result + userId;
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (phoneNumbers == null) {
+			if (other.phoneNumbers != null)
+				return false;
+		} else if (!phoneNumbers.equals(other.phoneNumbers))
+			return false;
+		if (userId != other.userId)
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+	
+	
+}
